677c550968ff9a34aa8e617df0c96d7488fafb0d
berial <9544316+berial@users.noreply.github.com>
2025-09-30 16:36:54 +0800

    Update file_type_detector.dart

--- lib/src/file_type_detector.dart
+++ lib/src/file_type_detector.dart
@@ -2,14 +2,13 @@
 
 import 'package:path/path.dart' as p;
 
-enum FileType { image, video, pdf, word, excel, csv, text, md }
+enum FileType { image, video, word, excel, csv, text, md }
 
 /// Detect the file type of the given file path.
 ///
 /// The function supports the following file types:
 /// - image: .jpg, .jpeg, .png, .gif, .bmp, .tiff
 /// - video: .mp4, .avi, .mov, .mkv
-/// - pdf: .pdf
 /// - word: .doc, .docx
 /// - excel: .xls, .xlsx
 /// - csv: .csv
@@ -23,8 +22,6 @@ FileType? detectFileType(String path) {
     return FileType.image;
   } else if (<String>['.mp4', '.avi', '.mov', '.mkv'].contains(extension)) {
     return FileType.video;
-  } else if (extension == '.pdf') {
-    return FileType.pdf;
   } else if (<String>['.doc', '.docx'].contains(extension)) {
     return FileType.word;
   } else if (<String>['.xls', '.xlsx'].contains(extension)) {
